9535c95b3e8c4e91cfefeb128fef5b2b01d1f7dd
Nik Tripp <nicholas.tripp@gmail.com>
2018-02-12 22:26:39 -0500

    creating the class

---
+++ HW02/Models/Tasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW02.Models
+{
+    public class Tasks
+    {
+        /// <summary>
+        /// This is the name of the task
+        /// It is required
+        /// takes a string
+        /// </summary>
+        public string TaskName { get; set; }
+        /// <summary>
+        /// flag if task is completed or not
+        /// this is required
+        /// </summary>
+        public bool IsCompleted { get; set; }
+        /// <summary>
+        /// Due data of the task it is required
+        /// takes type string
+        /// </summary>
+        public string DueDate { get; set; }
+    }
+}
